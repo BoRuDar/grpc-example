0496f1aeaa95d32aef30cf073c92693e80f3915f
Bohdan Darahan <bohdan.darahan@nordcloud.com>
2020-12-03 18:06:14 +0100

    Update python cli

--- cmd/py_client/main.py
+++ cmd/py_client/main.py
@@ -3,12 +3,12 @@ import api_pb2_grpc
 import grpc
 
 
-def run():
+def run(a: float, b: float):
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = api_pb2_grpc.CalcStub(channel)
-        response = stub.Calculate(api_pb2.Request(a=1.2, b=3.3))
+        response = stub.Calculate(api_pb2.Request(a=a, b=b))
     print("client received: ", response.result)
 
 
 if __name__ == '__main__':
-    run()
+    run(13.4, 34.4)
